3c759cb283d4bae0d046076bd560eb2935fdb22d
seyeonn <ansse17@gmail.com>
2021-04-30 23:26:09 +0900

    Repository Interface commit

--- src/test/java/com/jpa/bookmanager/repository/UserRepositoryTest.java
+++ src/test/java/com/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.jpa.bookmanager.repository;
 
 import com.jpa.bookmanager.domain.User;
+import com.jpa.bookmanager.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,6 +19,8 @@ class UserRepositoryTest {
     void crud() {       // create, read, update, delete
         userRepository.save(new User());    // user를 생성해서 저장. (NoArgsConstructor로 들어감.)
 
-        System.out.println(">>> " + userRepository.findAll());     // user 테이블에 있는 모든 데이터를 list 형태로 가져옴.
+        userRepository.findAll().forEach(System.out::println);
+        //System.out.println(">>> " + userRepository.findAll());      user 테이블에 있는 모든 데이터를 list 형태로 가져옴.
     }
+
 }
---
+++ src/test/resources/data.sql
@@ -0,0 +1,14 @@
+call next value for hibernate_sequence;
+insert into user ('id', 'name', 'email', 'created_at', 'updated_at') values (1, 'seco', 'asy@fc.com', now(), now());
+
+call next value for hibernate_sequence;
+insert into user ('id', 'name', 'email', 'created_at', 'updated_at') values (2, 'denis', 'denis@fc.com', now(), now());
+
+call next value for hibernate_sequence;
+insert into user ('id', 'name', 'email', 'created_at', 'updated_at') values (3, 'sophia', 'sophia@fc.com', now(), now());
+
+call next value for hibernate_sequence;
+insert into user ('id', 'name', 'email', 'created_at', 'updated_at') values (4, 'james', 'james@sc.com', now(), now());
+
+call next value for hibernate_sequence;
+insert into user ('id', 'name', 'email', 'created_at', 'updated_at') values (5, 'seco', 'asy@anotherc.com', now(), now());
