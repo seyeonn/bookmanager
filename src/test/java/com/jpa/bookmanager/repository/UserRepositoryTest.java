80cd2e2e2687024e4def25ddc6b920e5b4826224
seyeonn <ansse17@gmail.com>
2021-04-30 23:46:59 +0900

    Repository Interface commit

--- src/test/java/com/jpa/bookmanager/repository/UserRepositoryTest.java
+++ src/test/java/com/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -2,11 +2,18 @@ package com.jpa.bookmanager.repository;
 
 import com.jpa.bookmanager.domain.User;
 import com.jpa.bookmanager.repository.UserRepository;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,10 +24,23 @@ class UserRepositoryTest {
 
     @Test
     void crud() {       // create, read, update, delete
-        userRepository.save(new User());    // user를 생성해서 저장. (NoArgsConstructor로 들어감.)
+        User user = userRepository.findById(1L).orElse(null);
+        System.out.println(user);
+        // User user = userRepository.getOne(1L);
 
-        userRepository.findAll().forEach(System.out::println);
-        //System.out.println(">>> " + userRepository.findAll());      user 테이블에 있는 모든 데이터를 list 형태로 가져옴.
+        /* User user1 = new User("jack", "jack@naver.com");
+        User user2 = new User("steve", "steve@naver.com");
+
+        userRepository.saveAll(Lists.newArrayList(user1, user2));
+
+        List<User> users = userRepository.findAll();
+
+        users.forEach(System.out::println); */
+        // List<User> users = userRepository.findAllById(Lists.newArrayList(1L, 3L, 5L));  id가 1,3,5인 데이터 조회.
+        // List<User> users = userRepository.findAll(Sort.by(Sort.Direction.DESC, "name"));  이름 정렬
+        // userRepository.save(new User());     user를 생성해서 저장. (NoArgsConstructor로 들어감.)
+        // userRepository.findAll().forEach(System.out::println);
+        // System.out.println(">>> " + userRepository.findAll());      user 테이블에 있는 모든 데이터를 list 형태로 가져옴.
     }
 
 }
